de64837d1389411320f9b596e693d5ddf24df303
Melvin <chen.melv@husky.neu.edu>
2019-12-18 11:27:37 -0800

    gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+module.exports = {
+    plugins: [
+        {
+            resolve: "gatsby-source-graphql",
+            options: {
+                typeName: "TS",
+                fieldName: "takeshape",
+                // Url to query from
+                url: `https://api.takeshape.io/project/${process.env.TAKESHAPE_PROJECT}/graphql`,
+                // HTTP headers
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${process.env.TAKESHAPE_TOKEN}`,
+                },
+                // Additional options to pass to node-fetch
+                fetchOptions: {},
+            },
+        },
+    ]
+};
