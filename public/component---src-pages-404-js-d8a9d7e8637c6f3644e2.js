8f54cbaf3f3df534b69fac0d529a82b2da411713
Melvin <chen.melv@husky.neu.edu>
2019-12-18 22:08:25 -0800

    remove public directory

--- public/component---src-pages-404-js-d8a9d7e8637c6f3644e2.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),o=t.n(a),u=t("Bl7J"),l=t("vrFN");e.default=function(){return o.a.createElement(u.a,null,o.a.createElement(l.a,{title:"404: Not found"}),"Page not found")}}}]);
-//# sourceMappingURL=component---src-pages-404-js-d8a9d7e8637c6f3644e2.js.map
--- public/styles-eb84f506ad07f41dfdbe.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"8ypT":function(n,p,w){},TpwP:function(n,p,w){}}]);
-//# sourceMappingURL=styles-eb84f506ad07f41dfdbe.js.map
